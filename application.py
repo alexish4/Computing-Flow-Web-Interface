6ee0bc3b3f185b72c1941df1416ebaa1cf8a6f8b
alexish4 <alexish4@cpp.edu>
2024-08-09 16:51:19 -0700

    Dep

--- application.py
+++ application.py
@@ -73,6 +73,6 @@ def compute_flow_betweenness():
     return jsonify({'betweenness_score': betweenness_score})
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    # port = int(os.environ.get("PORT", 5000))
-    # app.run(host='0.0.0.0', port=port)
+    #app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
