cb47d510ff5c6fc16a00652886fdb93757c69448
alexish4 <alexish4@cpp.edu>
2024-08-20 14:17:55 -0700

    We got working tabs!

--- static/graphs.js
+++ static/graphs.js
@@ -451,3 +451,21 @@ function drawColorScale() {
         .attr("transform", `translate(${width}, 0)`)
         .call(axis);
 }
+
+// Function to open a tab
+function openTab(evt, tabName) {
+    let i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Open the first tab by default
+document.querySelector(".tablinks").click();
