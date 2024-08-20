3085b7d4fd6809a0ecb2e94799c19d248cbc9def
alexish4 <alexish4@cpp.edu>
2024-08-20 14:45:05 -0700

    Test heroku different tier

--- application.py
+++ application.py
@@ -261,6 +261,6 @@ def histograms(path_lengths, path_lengths2):
     return img_data, img_data2
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    # port = int(os.environ.get("PORT", 5000))
-    # app.run(host='0.0.0.0', port=port)
+    #app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
