08a30ca6b5a2e493501449c698a79959435f7424
alexish4 <alexish4@cpp.edu>
2024-08-23 15:22:22 -0700

    Going to try to run on server now

--- application.py
+++ application.py
@@ -386,6 +386,6 @@ def generateTopPaths2(graph, k, so, si):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    # port = int(os.environ.get("PORT", 5000))
-    # app.run(host='0.0.0.0', port=port)
+    #app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
